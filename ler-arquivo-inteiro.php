ed943048e2e1530836a171bde459ef62a5caf855
mateuslph <mlptche@gmail.com>
2022-10-12 19:29:19 -0300

    adicionado observação do limite de leitura do arquivo

--- ler-arquivo-inteiro.php
+++ ler-arquivo-inteiro.php
@@ -3,6 +3,6 @@
 $arquivo = fopen('cursos.txt', 'r');
 
 $tamanhoDoArquivo = filesize('cursos.txt'); // faz a leitura do tamanho em bytes do arquivo
-$cursos = fread($arquivo, $tamanhoDoArquivo);
+$cursos = fread($arquivo, $tamanhoDoArquivo); // limite de leitura neste modelo eh de 128MB
 echo $cursos;
 fclose($arquivo);
